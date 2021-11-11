3be383227a357897b0dc9b43036c249681120ae1
NetAcad DEVASC <changeme@example.com>
2021-11-11 12:07:50 +0000

    Web app added navigation and design.

---
+++ app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp rabbit-app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY rabbit-app.py /home/myapp/" >> tempdir/Dockerfile
+
+echo "EXPOSE 2828" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/rabbit-app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t rabbitapp .
+docker run -t -d -p 2828:2828 --name rabbitapprunning rabbitapp
+docker ps -a
---
+++ rabbit-app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('welcome.html')
+
+@app.route('/about')
+def more():
+    return render_template('more.html')
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2828)
