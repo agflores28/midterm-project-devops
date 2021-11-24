9582dd5531ce5e4e4ab4916283c79ddfeebe6301
agflores28 <ageorgeflore@gmail.com>
2021-11-24 21:59:00 +0800

    fixed lines

--- rabbit-app.py
+++ rabbit-app.py
@@ -20,5 +20,9 @@ def members():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2828)
