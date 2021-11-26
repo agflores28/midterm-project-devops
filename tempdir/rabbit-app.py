d416320f6d3ea9e5e4e9f62c2ab7690262d6884a
NetAcad DEVASC <changeme@example.com>
2021-11-26 15:47:57 +0000

    new docker

---
+++ tempdir/rabbit-app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('welcome.html')
+
+@app.route('/about')
+def more():
+    return render_template('more.html')
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2828)
