30e88f6dab93b3e72a32f57660cf63d7d98facc5
NetAcad DEVASC <changeme@example.com>
2021-11-27 21:13:05 +0000

    new doccker

--- tempdir/rabbit-app.py
+++ tempdir/rabbit-app.py
@@ -23,32 +23,48 @@ def more():
 def members():
     return render_template('members.html')
 
-@app.route('/login')
+@app.route('/login', methods = ["GET", "POST"])
 def login():
-    return render_template('login.html')
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    else:
+        user_name = request.form['user_name']
+        password = request.form['password']
+        rec = tuple(user_collection.find({'$and':[{'user_name': user_name, 'password': password}]}).limit(1) )
+        if rec:
+            return render_template('welcome.html',Registered = True, user_name = rec[0]['user_name'])
+
+        else:
+            return render_template('login.html', Invalid = True)
+
 
 @app.route('/signup', methods = ["GET", "POST"])
 def signup():
 
-    if request.methods == 'POST':
-        first_name = request.form['fname']
-        last_name = request.form['lname']
-        user_name = request.form['user']
-        password = request.form['pass']
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        user_name = request.form['user_name']
+        password = request.form['password']
         new_user = {
-        "fname": first_name,
-        "lname": last_name,
-        "user": user_name,
-        "pass": password
-    }
-        if first_name == '' or last_name == '' or user_name == '' or password == '':
-            print("Invalid!")
+        "first_name": first_name,
+        "last_name": last_name,
+        "user_name": user_name,
+        "password": password
+        }
+        if tuple(user_collection.find({'user_name': user_name}).limit(1)):
+            return render_template('signup.html', Existing = True)
 
         else:
-            user_collection.inser_one(new_user)
-            print("Inserted!")
+            user_collection.insert_one(new_user)
+            return render_template('signup.html',Success = True, user_name = user_name)
 
-    return render_template('signup.html')
+    else:
+        return render_template('signup.html')
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2828)
